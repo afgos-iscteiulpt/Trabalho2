78e29b239b07c023482ba614dc112f8309a444f0
MrAndrGodinho <MrAndrGodinho@LAPTOP-P8P29RAN>
2018-11-05 17:58:19 +0000

    added smoothing

--- Trabalho2/src/FileReader/TextFileModifier.java
+++ Trabalho2/src/FileReader/TextFileModifier.java
@@ -16,19 +16,18 @@ import com.google.common.collect.Multiset;
 import com.google.common.collect.Table;
 
 public class TextFileModifier {
-	
+
 	public static void readFile(File file, DataProcessor processor) {
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-		    String line;
-		    while ((line = br.readLine()) != null) {
-		       processor.processQuestion(line);
-		    }
+			String line;
+			while ((line = br.readLine()) != null) {
+				processor.processQuestion(line);
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void writeUnigramFile(String fileName, Multiset<String> unigram) {
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
@@ -38,28 +37,63 @@ public class TextFileModifier {
 			}
 			writer.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void writeBigramFile(String fileName, Table<String, String, Integer> bigram) {
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-			for(String row : bigram.rowKeySet()) 
-				for(String column : bigram.columnKeySet())
+			for (String row : bigram.rowKeySet())
+				for (String column : bigram.columnKeySet())
 					if (bigram.get(row, column) != null)
-					writer.write(row + " " + column + " " + bigram.get(row, column) + "\n");
+						writer.write(row + " " + column + " " + bigram.get(row, column) + "\n");
 			writer.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
+	public static void writeUnigramSmoothFile(String fileName, Multiset<String> unigram) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+			Set<String> uniqueSet = unigram.elementSet();
+			for (String s : uniqueSet) {
+				Integer value = unigram.count(s)+unigram.elementSet().size();
+				writer.write(s + " " + value + "\n");
+			}
+			writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void writeBigramSmoothFile(String fileName, Table<String, String, Integer> bigram) {
+		Integer value;
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+			for (String row : bigram.rowKeySet()) {
+				for (String column : bigram.columnKeySet()) {
+					if (bigram.get(row, column) != null) {
+						value = bigram.get(row, column)+1;
+					} else {
+						value = 1;
+					}
+						writer.write(row + " " + column + " " + value + "\n");
+				}
+			}			
+			writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
